74e90e28cb99df820f5b61fc45dffbb90ac26c86
FrancoPagnotta <francoapagnotta@gmail.com>
2021-10-26 08:08:10 -0300

    card player component created

---
+++ src/app/shared/components/cardplayer/cardplayer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardplayer',
+  templateUrl: './cardplayer.component.html',
+  styleUrls: ['./cardplayer.component.css']
+})
+export class CardplayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MediaplayerComponent } from './components/mediaplayer/mediaplayer.component';
 import { HeaderuserComponent } from './components/headeruser/headeruser.component';
+import { CardplayerComponent } from './components/cardplayer/cardplayer.component';
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
     MediaplayerComponent,
-    HeaderuserComponent
+    HeaderuserComponent,
+    CardplayerComponent
   ],
   imports: [
     CommonModule
