b97793632777a419758a9fc098eb551b71c7f79a
FrancoPagnotta <francoapagnotta@gmail.com>
2021-10-25 23:31:31 -0300

    modules auth, home, tracks, favorites, history and shared created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from '@modules/home/pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [ // Aca declaramos las rutas, en este array de rutas
+  { 
+    path: 'auth', 
+    loadChildren:()=> import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  { 
+    path: '', 
+    component: HomepageComponent,
+    loadChildren:()=> import('./modules/home/home.module').then(m => m.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // Declaraciones, Directivas, Pipes
+    AppComponent, 
   ],
-  imports: [
+  imports: [ // Solo se importan otros modulos 
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
